d6132ea172236efa5282e9d94c9bf51fb254cb7b
Invok <44592591+InvokFung@users.noreply.github.com>
2024-02-19 09:35:48 +0800

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const ejs = require('ejs');
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/", express.static('public'));
 
