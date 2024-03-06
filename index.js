0b56ead66e8e9ad24a9ba8aacb000d56e72e58a1
InvokFung <44592591+InvokFung@users.noreply.github.com>
2024-03-07 02:37:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -91,18 +91,19 @@ app.post('/process_image/:userId/:projectId', upload.array('images'), async (req
         }
 
         console.log("Reconstruction process finished.")
-        console.log("Server accessing result...")
-        try {
-            const npyBody = (await s3Download(req)).Body;
-            // Set response headers
-            res.setHeader('Content-Type', 'application/octet-stream');
-            res.setHeader('Content-Disposition', 'attachment; filename="accumulated_numpy.npy"');
-            // Send the file data as a binary stream
-            res.send(npyBody);
-            console.log("Result sent to client.")
-        } catch (err) {
-            console.error(err);
-        }
+        res.send("Success");
+        // console.log("Server accessing result...")
+        // try {
+        //     const npyBody = (await s3Download(req)).Body;
+        //     // Set response headers
+        //     res.setHeader('Content-Type', 'application/octet-stream');
+        //     res.setHeader('Content-Disposition', 'attachment; filename="accumulated_numpy.npy"');
+        //     // Send the file data as a binary stream
+        //     res.send(npyBody);
+        //     console.log("Result sent to client.")
+        // } catch (err) {
+        //     console.error(err);
+        // }
     });
 });
 
